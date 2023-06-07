0a11b4a0a1806739ac4e7d4ac4a6ac0ffc260d28
BaiMeow <baishu2003@outlook.com>
2023-06-07 13:40:46 +0800

    feat: bounce command

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of wg-quick-op",
+	Long:  `All software has versions. This is wg-quick-op's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Wireguard Quick for Openwrt v0.0.2")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
