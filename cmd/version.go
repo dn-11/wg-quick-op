91b304ad0b6cdbe410c6b4395161d4164db8c6b6
BaiMeow <baishu2003@outlook.com>
2023-12-01 21:40:48 +0800

    build: go release

--- cmd/version.go
+++ cmd/version.go
@@ -6,13 +6,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version = ""
+
 // versionCmd represents the version command
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of wg-quick-op",
 	Long:  `All software has versions. This is wg-quick-op's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Wireguard Quick for Openwrt v0.1.1")
+		fmt.Printf("Wireguard Quick for Openwrt %s\n", version)
 	},
 }
 
