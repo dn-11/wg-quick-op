646f17710e6b232a81458893748da77c54cb49d9
Ljcbaby <46277145+ljcbaby@users.noreply.github.com>
2025-05-21 01:46:37 +0800

    feat: random ListenPort

--- conf/config_test.go
+++ conf/config_test.go
@@ -4,4 +4,9 @@ import "testing"
 
 func TestParseConfig(t *testing.T) {
 	Init("config-sample.toml")
+	t.Logf("config: %+v", DDNS)
+	t.Logf("config: %+v", StartOnBoot)
+	t.Logf("config: %+v", EnhancedDNS)
+	t.Logf("config: %+v", Wireguard)
+	t.Logf("config: %+v", Log)
 }
